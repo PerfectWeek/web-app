d6d0a3020fa722c8c998747c4e3d049b391a64bf
Bento <mohamed.bentorcha@epitech.eu>
2019-04-12 01:42:16 -0700

    changed interface of friend list and added friend request

--- src/app/pages/Friends/friends.ts
+++ src/app/pages/Friends/friends.ts
@@ -20,12 +20,26 @@ export class FriendsComponent {
 	{ name: 'Seb'},
     ];
 
+    rqst: Friends[] = [
+	{ name: 'Amin Boule Blanche'},
+	{ name: 'Rotaru'},
+	{ name: 'Reitaru Uzumaki'},
+	{ name: 'underscore i'},
+	{ name: 'Guillaume BEUNARD'},
+	{ name: 'GIAUMRE BAIGNOIRE'},
+	{ name: 'Mathias Alphabet Arabe'},
+	{ name: 'Julien de la Montagne'},
+    ]
+
+    nb: number;
+    
     x: Friends = {name: "Christian"};
     constructor(private profileSrv: ProfileService) {
 	
     }
 
     ngOnInit() {
+	this.nb = Object.keys(this.rqst).length;
 	this.profileSrv.userProfile$.subscribe(user => {
 	    this.user = user;
 	}, (error) => {console.log('error => ', error)});
@@ -35,11 +49,22 @@ export class FriendsComponent {
 	//const index: number = this.friends.indexOf(fr);
 	//console.log("INDEX : " + index);
 	this.friends = this.friends.filter(item => item !== fr);
-	console.log("function deleteFriend called ici");
+	console.log("function deleteFriend called");
     }
 
     addFriend() {
 	this.friends.push(this.x);
 	console.log("function addFriend called");
+    }
+
+    deleteRequest(fr: Friends){
+	this.nb = this.nb - 1;
+	this.rqst = this.rqst.filter(item => item !== fr);
+	console.log("function deleteRequest called");
+    }
+
+    moveRequestToFriend(fr: Friends){
+	this.friends.push(fr);
+	this.deleteRequest(fr);
     }
 }
