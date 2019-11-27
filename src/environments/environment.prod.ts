57897e2f048ee1ab4e803da8c0eb5fc7210e0d6c
boulen_b <amin.boulenoir@epitech.eu>
2019-11-27 23:36:15 +0100

    Env prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    url: "https://api.perfect-week.pw/",
-    apiEndpoint: 'https://api.perfect-week.pw/',
+    url: "https://perfect-week-api.herokuapp.com/",
+    apiEndpoint: 'https://perfect-week-api.herokuapp.com/',
     appName: 'PerfectWeek',
     google_client_id: '106749751777-o32qljvulu0vnpgpaqe3mmqfei520p5n.apps.googleusercontent.com',
     facebook_client_id: '850667108631602'
